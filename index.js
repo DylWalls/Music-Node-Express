fe0d8fadc56f48c29d6f982ba9c90038fd57825e
Dylan Walls <dylanwalls410@gmail.com>
2021-08-03 17:21:26 -0600

    Fixed errors on code.

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const repoContext = require('./repository/repository-wrapper');
+const cors = require('cors');
 const { validateSong } = require('./middleware/songs-validation');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 app.listen(9000, function () {
-    console.log("Server started. Listening on port9000.");
+    console.log("Server started! Listening on port9000.");
     });
+
+app.get('/api/songs/', (req,res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
     
 app.get('/api/songs/:id', (req, res) => {
         const id = req.params.id;
-        const songs = repoContext.songs.findAllSongs();
-        return res.send(songs);
+        const song = repoContext.songs.findSongById(id);
+        return res.send(song);
 });
 
 app.post('/api/songs', [validateSong], (req, res) => {
